f14aae79bf906f4d5c72c5a76b3b9479850999cf
gabrielecruciani <138598344+BoiZze@users.noreply.github.com>
2023-09-14 17:30:54 +0200

    Update cursor.js

--- js/cursor.js
+++ js/cursor.js
@@ -1,6 +1,9 @@
 var cursor = document.querySelector('.cursor');
 var cursorinner = document.querySelector('.cursor2');
 var a = document.querySelectorAll('a');
+
+var videos = document.querySelectorAll('iframe');
+
 document.addEventListener('mousemove', function(e){
 var x = e.clientX;
 var y = e.clientY;
@@ -20,3 +23,23 @@ item.addEventListener('mouseleave', () => {
 cursor.classList.remove('hover');
 });
 })
+
+
+videos.forEach(video => {
+    video.addEventListener('mouseover', () => {
+      cursor.style.display = 'none'; // Nascondi il cursore personalizzato sopra il video
+    });
+    video.addEventListener('mouseleave', () => {
+      cursor.style.display = 'block'; // Riporta il cursore personalizzato quando il cursore esce dal video
+    });
+  
+    // Aggiungi un gestore per fare clic sui video incorporati
+    video.addEventListener('click', () => {
+      // Simula il clic sul video
+      if (video.paused) {
+        video.play();
+      } else {
+        video.pause();
+      }
+    });
+  });
